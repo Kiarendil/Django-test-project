603f88ebb9b5d89688b43f846ad788fde6673ac7
Kiarendil <Ivanov.KM@phystech.edu>
2018-01-11 02:47:09 +0300

    minor test push

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='core/base.html'), name='index'),
     url(r'^index/$', TemplateView.as_view(template_name='core/base.html')),
     url(r'^login/$', login, {'template_name': 'core/login.html'}, name="login"),
-    url(r'^logout/$', logout, {'template_name': 'core/logout.html'}, name="logout")
+    url(r'^logout/$', logout, {'template_name': 'core/logout.html'}, name="logout"),
 ]
