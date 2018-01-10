94c3a28b8c29fb8ec4aae47af9e9f9e4aee2a923
Kiarendil <Ivanov.KM@phystech.edu>
2018-01-11 02:25:37 +0300

    push

--- blog/urls.py
+++ blog/urls.py
@@ -17,11 +17,11 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from core import views
+admin.site.site_header = 'Тестовый сайт'
+admin.site.index_title = "Администрирование тестового сайта"
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='core/base.html'), name='index'),
-    url(r'^index/$', TemplateView.as_view(template_name='core/base.html')),
     url(r'^blogs/', include('blogs.urls')),
+    url(r'', include('core.urls')),
 ]
---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.views.generic import TemplateView
+from django.contrib.auth.views import login, logout
+
+urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='core/base.html'), name='index'),
+    url(r'^index/$', TemplateView.as_view(template_name='core/base.html')),
+    url(r'^login/$', login, {'template_name': 'core/login.html'}, name="login"),
+    url(r'^logout/$', logout, {'template_name': 'core/logout.html'}, name="login")
+]
